2832376ce2db9430b63ea4717fad9d939492f122
debnsuma <debnsuma@amazon.com>
2022-11-21 22:02:03 -0500

    adding the code for YT12, 14 15 and 16

---
+++ GFG/recursion/YT_16_permutation_with_letter_case.py
@@ -0,0 +1,40 @@
+import string
+
+
+class Solution:
+    count = 0
+    def substring(self, input_string, output_string):
+
+        # My base condition
+        if len(input_string) == 0:
+            print(output_string)
+            self.count += 1
+            return self.count
+
+        if input_string[0] in string.ascii_letters:
+            # When we are considering UPPER case of the first char
+            self.substring(input_string[1:], output_string + input_string[0].upper())
+
+
+            # When we are considering LOWER case of the first char
+            self.substring(input_string[1:], output_string + input_string[0].lower())
+        else:
+            self.substring(input_string[1:], output_string + input_string[0])
+
+        return self.count
+
+def main():
+    T = int(input())
+
+    while T > 0:
+        input_string = input()
+        output_string = ""
+        ob = Solution()
+        print(ob.substring(input_string, output_string))
+        T -= 1
+
+
+if __name__ == '__main__':
+    main()
+
+
