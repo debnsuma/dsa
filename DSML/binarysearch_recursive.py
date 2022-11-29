f255c5a9db857f870806057f5a5c3e7e89cce67c
debnsuma <debnsuma@amazon.com>
2022-11-29 10:06:42 -0800

    binary search

--- DSML/binarysearch_recursive.py
+++ DSML/binarysearch_recursive.py
@@ -2,7 +2,7 @@ def recur_search(key, values, lower, upper):
 
     # base case
     # print(lower, upper)
-    if lower >= upper:
+    if lower > upper:
         return False
 
     mid = (upper + lower)//2
@@ -17,12 +17,27 @@ def recur_search(key, values, lower, upper):
         return recur_search(key, values, lower, upper)
 
 
+def recur_search2(key, values, lower, upper):
+
+    while lower <= upper:
+
+        mid = (upper + lower)//2
+        if values[mid] == key:
+            return True
+        elif key < values[mid]:
+            upper = mid - 1
+        else:
+            lower = mid + 1
+
+    else:
+        return False
 
 
 
-key = 2
-values = [1,1,1,1,1]
+
+key = 1
+values = [0,2,2,3]
 lower = 0
 upper = len(values)
-print(recur_search(key, values, lower, upper))
+print(recur_search2(key, values, lower, upper-1))
 
