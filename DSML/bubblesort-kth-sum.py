ed9be38d3bf5d700368650b61bbf0e50c4e88cda
debnsuma <debnsuma@amazon.com>
2022-12-01 18:57:02 -0800

    bubble sort

--- DSML/bubblesort-kth-sum.py
+++ DSML/bubblesort-kth-sum.py
@@ -1,23 +1,20 @@
-def bubble_sort(a):
-    n = len(a)
+def bubble_sort(a, k):
+    n = len(a[:k])
+    print(n)
     for i in range(n):
         flag = False
         for j in range(0, n - i - 1):
             if a[j] > a[j+1]:
                 a[j + 1], a[j] = a[j], a[j + 1]
                 flag = True
-        # if not flag:
-        #     break
-#            print(a, a[::-1][0] + a[::-1][-1])
-            print(a, a[0] + a[-1])
+        if not flag:
+            break
+
+    return a[0] + a[-1]
 
 
-    return a[::-1]
-
-
-#arr = "64 34 25 12 22 11 90".split()
-arr = "46 86 55 13 19 40 80 78".split(" ")
-#arr = "54 75 58 96 113 54 667".split(" ")
+arr = "54 75 58 96 113 54 667".split(" ")
 arr = [int(i) for i in arr]
-bubble_sort(arr)
+bubble_sort(arr, 5)
 
+#%%
